a73d55722889932e729ef61b3ac3a7a0160ac931
DaScOrPiO <dunni82@yahoo.com>
2023-09-09 21:09:01 +0100

    FIX error

--- index.js
+++ index.js
@@ -4,29 +4,15 @@ const moment = require("moment");
 
 const port = process.env.PORT || 3000;
 
-function getCurrentUtcTime() {
-  const currentTime = moment.utc();
-  const allowedDifference = 2000;
-  const serverTime = moment();
-  const timeDifference = Math.abs(currentTime.diff(serverTime));
-
-  if (timeDifference <= allowedDifference) {
-    return currentTime.format("YYYY-MM-DD HH:mm:ss");
-  } else {
-    return null;
-  }
-}
-const now = new Date();
-const timeUtc = now.toISOString().split(".")[0] + "Z";
-const day = moment().format("dddd");
-// const utcTime = getCurrentUtcTime();
-const githubFileUrl = "https://github.com/DaScOrPiO/Taskone/blob/main/index.js";
-const repoUrl = "https://github.com/DaScOrPiO/Taskone";
-
 app.get("/api", (req, res) => {
   const slack_name = req.query.slack_name || "DaScOrPiO";
   const track = req.query.track || "backend";
-  const statusCode = res.status(200).statusCode;
+  const now = new Date();
+  const timeUtc = now.toISOString().split(".")[0] + "Z";
+  const day = moment().format("dddd");
+  const githubFileUrl =
+    "https://github.com/DaScOrPiO/Taskone/blob/main/index.js";
+  const repoUrl = "https://github.com/DaScOrPiO/Taskone";
 
   const values = {
     slack_name: slack_name,
@@ -35,10 +21,10 @@ app.get("/api", (req, res) => {
     track: track,
     github_file_url: githubFileUrl,
     github_repo_url: repoUrl,
-    status_code: statusCode,
+    status_code: 200,
   };
 
-  res.json(values);
+  res.status(200).json(values);
 });
 
 app.listen(port, () => console.log(`Server started at port: ${port}`));
